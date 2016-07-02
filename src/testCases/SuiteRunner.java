e6ef0120afbb1cdf29562a345df6c32b0da60bf7
Debarnab Banerjee <Debarnab Banerjee>
2016-07-02 21:06:26 +0200

    second commit

---
+++ src/testCases/SuiteRunner.java
@@ -0,0 +1,12 @@
+package testCases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({LoginTestCase.class,bookFlightTestCase.class}
+)
+public class SuiteRunner {
+
+}
